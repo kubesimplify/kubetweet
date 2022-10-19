db465fd3480fc0dc80c5f91fdb9c4de5b1167314
Your Namesanyamjain04 <sanyamjainsj04@gmail.com>
2022-10-19 15:28:36 +0530

    added threads feature

--- frontend/src/components/Thread.jsx
+++ frontend/src/components/Thread.jsx
@@ -6,24 +6,24 @@ import Textarea from "./Textarea";
 import { backendUrl } from "../constants/constant";
 
 export default function Thread() {
-  // Initialise the state as an empty array
-  const [state, setState] = useState([{ text: "" }]);
+  // Initialise the tweets as an empty array
+  const [tweets, setTweets] = useState([{ text: "" }]);
   const [tweetLoading, setTweetLoading] = useState(false);
 
   // When the `+` button is clicked, add a new
-  // empty object to the state
-  function addRow(e) {
+  // empty object to the tweets
+  function addTextarea(e) {
     const object = { text: "" };
-    setState([...state, object]);
+    setTweets([...tweets, object]);
   }
 
   // When the `-` button is clicked,
   // delete the last object in array
-  function deleteRow(e) {
-    if (state.length !== 1) {
-      const values = [...state];
+  function deleteTextarea(e) {
+    if (tweets.length !== 1) {
+      const values = [...tweets];
       values.splice(-1);
-      setState(values);
+      setTweets(values);
     }
   }
 
@@ -38,63 +38,63 @@ export default function Thread() {
       // Get the name and value from the changed element
       const { name, value } = e.target;
 
-      // Copy the state
-      const copy = [...state];
+      // Copy the tweets
+      const copy = [...tweets];
 
-      // Create a new object using the object in state
+      // Create a new object using the object in tweets
       // at index === id, and update the correct property
-      const obj = { ...state[id], [name]: value };
+      const obj = { ...tweets[id], [name]: value };
 
-      // Add that object to the state copy
+      // Add that object to the tweets copy
       copy[id] = obj;
 
-      // Finally update the state
-      setState(copy);
+      // Finally update the tweets
+      setTweets(copy);
     }
   }
 
   // We now create some rows by mapping
   // over the data and returning an array
   // of components which have an id based
-  // on their position in the state array, some
+  // on their position in the tweets array, some
   // data contained in the object, and the handler
-  function createTextarea(state) {
-    return state.map((obj, i) => {
+  function createTextarea(tweets) {
+    return tweets.map((obj, i) => {
       return <Textarea key={i} id={i} data={obj} handleChange={handleChange} />;
     });
   }
 
   const postThread =  () => {
-    for (let i = 0; i < state.length; i++) {
-      if (state[i].text.length < 3) {
+    for (let i = 0; i < tweets.length; i++) {
+      if (tweets[i].text.length < 3) {
         toast.warn(`Length of Tweet ${i + 1} is too short.`);
         return;
       }
     }
     setTweetLoading(true);
-     fetch(backendUrl + `/thread?threadData=${JSON.stringify(state)}`)
+     fetch(backendUrl + `/thread?threadData=${JSON.stringify(tweets)}`)
       .then((res) => res.json())
       .then((data) => toast.success(data.text))
       .catch((err) => console.log(err))
       .finally(() => setTweetLoading(false));
 
-    setState([{ text: "" }]);
+    setTweets([{ text: "" }]);
   };
 
-  // Check to see if state has length, and then
+  // Check to see if tweets has length, and then
   // create the rows
   return (
     <>
-      {state?.length && createTextarea(state)}
+      {tweets?.length && createTextarea(tweets)}
 
       <div className="self-end">
-        {state?.length > 1 && (
-          <button className="text-xl mr-1" onClick={deleteRow}>
+        {tweets?.length > 1 && (
+          <button className="text-xl mr-1" onClick={deleteTextarea}>
             <GrSubtractCircle />
           </button>
         )}
-        {state[state.length - 1]?.text.trim().length > 1 && (
-          <button className="text-xl" onClick={addRow}>
+        {tweets[tweets.length - 1]?.text.trim().length > 1 && (
+          <button className="text-xl" onClick={addTextarea}>
             <GrAddCircle />
           </button>
         )}
