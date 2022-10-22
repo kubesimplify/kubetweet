0b5028ee656b76e741bcb1bed97916626e7af168
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2022-10-22 09:30:49 +0530

    added unfollow route

---
+++ backend/routes/unfollow.js
@@ -0,0 +1,27 @@
+const { Router } = require("express");
+const { twitterAccountId } = require("../config");
+const getRefreshClient = require("../utils/TwitterApi/refreshClient");
+const router = Router();
+
+router.get("/", async (req, res) => {
+
+    const refreshedClient = await getRefreshClient();
+    console.log(refreshedClient);
+  // Take the User Id from request
+  const { targetUserId } = req.query;
+
+  // unfollow with UserId
+  try {
+    const { data } = await refreshedClient.v2.unfollow(
+      twitterAccountId,
+      targetUserId
+    );
+      res.send(data);
+      console.log(data)
+  } catch (error) {
+    console.log(error);
+    res.send(error.data.errors[0].message);
+  }
+});
+
+module.exports = router;
