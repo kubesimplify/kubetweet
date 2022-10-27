906c520fc079a5cd35f08a6a836eab4fe70166c2
Thiyagarajan Ravichandran <thiyagarajanravi22@gmail.com>
2022-10-27 22:25:29 +0530

    Onboarded bookmark - backend

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,7 @@ app.use("/reply", require("./routes/reply"));
 app.use("/unfollow", require("./routes/unfollow"));
 app.use("/userid", require("./routes/userid"));
 app.use("/unlike", require("./routes/unlike"));
+app.use("/bookmark", require("./routes/bookmark"));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
