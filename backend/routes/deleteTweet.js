2b1e862d72ded8c6b60f10472dd3d01846e76dd7
Yash Gulati <yashgulatii007@gmail.com>
2022-10-28 12:36:30 +0530

    Handled Non Existing Tweet Error

--- backend/routes/deleteTweet.js
+++ backend/routes/deleteTweet.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { readTwitterClient } = require("../config");
 const getRefreshClient = require("../utils/TwitterApi/refreshClient");
 const router = Router();
 
@@ -8,12 +9,20 @@ router.get("/", async (req, res) => {
   // Take the tweet id from request
   const { id } = req.query;
 
-  try {
-    // Delete the tweet
-    const { data } = await refreshedClient.v2.deleteTweet(id);
-    res.send(data);
-  } catch (error) {
-    res.send(error.data.detail);
+  const data1 = await readTwitterClient.v2.singleTweet(id);
+
+  // Check if Tweet Exist
+
+  if (data1.data) {
+    try {
+      // Delete the tweet
+      const { data } = await refreshedClient.v2.deleteTweet(id);
+      res.send(data);
+    } catch (error) {
+      res.send(error.data.detail);
+    }
+  } else {
+    res.send("Tweet Does not Exist");
   }
 });
 
